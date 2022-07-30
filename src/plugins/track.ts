c130cebde48135b21e0bbe99d6ac53a2de12dfa0
max180643 <max180643@gmail.com>
2022-07-31 00:45:21 +0700

    :ambulance: get real ip

--- src/plugins/track.ts
+++ src/plugins/track.ts
@@ -9,13 +9,23 @@ const asyncTrack: FastifyPluginAsync = async (fastify, options) => {
       "onRequest",
       async (request: FastifyRequest, reply: FastifyReply) => {
         const { ip, url, headers } = request;
+        const fwdIp = headers["x-forwarded-for"];
         const userAgent = headers["user-agent"];
+        let realIp: string;
+
+        if (fwdIp) {
+          const list = (<string>fwdIp).split(",");
+          realIp = list[list.length - 1];
+        } else {
+          realIp = ip;
+        }
+
         fetch(`${CONFIG.TRACK_API_URL}/track`, {
           method: "POST",
           body: JSON.stringify({
             websiteName: CONFIG.UMAMI_WEBSITE_NAME,
             hostname: CONFIG.UMAMI_WEBSITE_DOMAIN,
-            ip,
+            ip: realIp,
             userAgent,
             url,
           }),
