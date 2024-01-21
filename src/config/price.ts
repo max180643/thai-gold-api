2ae83e81116e9ea3a1e40fa15f5123de893f459a
max180643 <max180643@gmail.com>
2024-01-21 15:03:56 +0700

    fix: :bug: wrong selector

--- src/config/price.ts
+++ src/config/price.ts
@@ -1,21 +1,21 @@
 const PRICE_CONFIG = {
   URL: "https://xn--42cah7d0cxcvbbb9x.com/",
   SELECTOR: {
-    DATE: "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(5) > td.span.bg-span.txtd.al-r",
+    DATE: "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(4) > td.span.bg-span.txtd.al-r",
     UPDATE_TIME:
-      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(5) > td.em.bg-span.txtd.al-r",
+      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(4) > td.em.bg-span.txtd.al-r",
     GOLD_BUY:
-      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(3) > td:nth-child(3)",
-    GOLD_SELL:
-      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(3) > td:nth-child(2)",
-    GOLD_BAR_BUY:
       "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(2) > td:nth-child(3)",
-    GOLD_BAR_SELL:
+    GOLD_SELL:
       "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(2) > td:nth-child(2)",
+    GOLD_BAR_BUY:
+      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(1) > td:nth-child(3)",
+    GOLD_BAR_SELL:
+      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(1) > td:nth-child(2)",
     PRICE_COMPARE_PREVIOUS:
-      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(4) > td:nth-child(3)",
+      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(3) > td.em.bg-em.al-l.g-n",
     PRICE_COMPARE_YESTERDAY:
-      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(4) > td:nth-child(1)",
+      "#rightCol > div.divgta.goldshopf > table > tbody > tr:nth-child(3) > td.span.bg-span.g-n",
   },
 };
 
