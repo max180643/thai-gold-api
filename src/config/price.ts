7c182e13bda7106897b792c10c7d630b77c851c5
max180643 <max180643@gmail.com>
2026-08-10 21:42:28 +0700

    fix: :bug: update price URL to point to the correct endpoint

--- src/config/price.ts
+++ src/config/price.ts
@@ -1,16 +1,11 @@
 const PRICE_CONFIG = {
-  URL: "https://classic.goldtraders.or.th/",
+  URL: "https://classic.goldtraders.or.th/default.aspx",
   SELECTOR: {
-    UPDATE_DATETIME:
-      "#DetailPlace_uc_goldprices1_lblAsTime > b > font",
-    GOLD_BUY:
-      "#DetailPlace_uc_goldprices1_lblOMBuy > b > font",
-    GOLD_SELL:
-      "#DetailPlace_uc_goldprices1_lblOMSell > b > font",
-    GOLD_BAR_BUY:
-      "#DetailPlace_uc_goldprices1_lblBLBuy > b > font",
-    GOLD_BAR_SELL:
-      "#DetailPlace_uc_goldprices1_lblBLSell > b > font",
+    UPDATE_DATETIME: "#DetailPlace_uc_goldprices1_lblAsTime > b > font",
+    GOLD_BUY: "#DetailPlace_uc_goldprices1_lblOMBuy > b > font",
+    GOLD_SELL: "#DetailPlace_uc_goldprices1_lblOMSell > b > font",
+    GOLD_BAR_BUY: "#DetailPlace_uc_goldprices1_lblBLBuy > b > font",
+    GOLD_BAR_SELL: "#DetailPlace_uc_goldprices1_lblBLSell > b > font",
   },
 };
 
